a4c30157dd77f56b5e26b9614946590d59b74a50
Arthur <arthurferreia1417@gmail.com>
2025-05-15 17:28:53 -0300

    FIX: adicionando validações de tipos na entrada dos dados

--- exercicio01.py
+++ exercicio01.py
@@ -1,8 +1,16 @@
 import math
 def produtoNumeros():
-    num1 = int(input("Digite um numero inteiro: "))
-    num2 = int(input("Digite um numero inteiro: "))
-    num3 = float(input("Digite um numero real: ").replace(",","."))
+    num1 = input("Digite um numero inteiro: ")
+    num2 = input("Digite um numero inteiro: ")
+    num3 = input("Digite um numero real: ").replace(",",".")
+
+    if isinstance(num1,int) != True or isinstance(num2, int) != True:
+        print("os dois numeros iniciais precisam ser inteiros")
+        return
+    
+    if isinstance(num3, float) != True:
+        print("os tericeiro numeros precisam ser real")
+        return
 
     produtoA = (num1 * 2) + (num2 / 2)
     produtoB = (num1 * 3) + num3
